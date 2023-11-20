cf0d65bedaa0fe5a99c894c7169f12f8a0a1dfda
aselker1 <mamatovaaselya28@gmail.com>
2023-11-20 18:00:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from "react";
 
-function App() {
+
+function App  ()  {
+  const [click, setClick] = useState(0);
+  const [num, setNum] = useState(10);
+  const [time, setTime] = useState(num * 1000);
+  const [result, setResult] = useState(null);
+  const [count, setCount] = useState(false);
+  
+  
+  useEffect(() => {
+    let timer;
+    
+    if (count && time > 0) {
+      timer = setInterval(() => {
+        setTime((prevTime) => (prevTime > 0 ? prevTime - 10 : prevTime));
+      }, 1);
+    } else if (count && time === 0) {
+      setResult(click / (num));
+    }
+    
+    return () => clearInterval(timer);
+  }, [time, num, count, click]);
+  
+  const displayTime = () => {
+    const seconds = Math.floor(time / 1000);
+    const milliseconds = (time % 1000).toString().padStart(3, '0');
+    return `${seconds}.${milliseconds} s`;
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1 >
+        {result !== null && `${result.toFixed(2)} кол-во кликов в секунду`}
+      </h1>    
+      <h1>{click}</h1>
+      <h1>
+        {displayTime()}
+      </h1>
+      
+      <button onClick = {() => {
+       setClick(click+1)
+       setCount(true);
+      }}>Click me</button>
+     
+      
+<button onClick = {() => {
+        setClick(0);
+        setTime(num * 1000);
+        setResult(null);
+        setCount(false);
+      }}>Reset</button>
     </div>
   );
-}
+};
+ 
 
 export default App;
